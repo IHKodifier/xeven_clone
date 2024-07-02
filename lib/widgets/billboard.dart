cb33e405b77703b47c9cc3e47f6061b3c9891cff
IHK_Office_Laptop <IHK_Office_Laptop>
2024-07-02 08:47:46 +0500

    billboard  implementation satisfasctory

--- lib/widgets/billboard.dart
+++ lib/widgets/billboard.dart
@@ -8,22 +8,40 @@ class Billboard extends StatefulWidget {
 }
 
 class _BillboardState extends State<Billboard> {
-  double spacing = 10;
-  double verticalSpacing = 100;
+  double maxTileWidth = 0;
+  double maxTileHeight = 0;
+  double spacing = 0;
+  double verticalSpacing = 0;
   Color hoverColor = Colors.transparent;
   Widget backgroundImage = Container();
-  // List<Widget> regions = [];
+  List<Widget> backgroundImages = [];
+  List<bool> isHovered = List.generate(
+    8,
+    (index) => false,
+  );
 
+  @override
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     backgroundImage = healthTech();
+    backgroundImages = [
+      healthTech(),
+      edTech(),
+      retail(),
+      aiDiag(),
+      greenTech(),
+      iot(),
+      finTech(),
+      hipaa()
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-  
+    maxTileWidth = (MediaQuery.of(context).size.width / 4.0) - 16;
+    maxTileHeight = (MediaQuery.of(context).size.height / 2.0) - 16;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
@@ -34,48 +52,71 @@ class _BillboardState extends State<Billboard> {
           const SectionHeader(
               title:
                   'Industries We Transformed with Innovative Software Solutions'),
-                  SizedBox(height:20 ,),
+          SizedBox(
+            height: 20,
+          ),
           Stack(
             children: [
               backgroundImage,
-mouseRegion(),
-              
+              mouseRegion(0,'Health Tech', 0, 0),
+              mouseRegion(1, 'Ed Tech',maxTileWidth, verticalSpacing),
+              mouseRegion(2, 'Retail',spacing + 2 * maxTileWidth, verticalSpacing),
+              mouseRegion(3, 'AI Diagnostics',spacing + 3 * maxTileWidth, verticalSpacing),
+              mouseRegion(4,'Green Tech', 0, verticalSpacing + maxTileHeight),
+              mouseRegion(5, 'IoT',spacing + 1 * maxTileWidth,
+                  verticalSpacing + maxTileHeight),
+              mouseRegion(6, 'Fin Tech',spacing + 2 * maxTileWidth,
+                  verticalSpacing + maxTileHeight),
+              mouseRegion(7, 'HIPAA Compliance',spacing + 3 * maxTileWidth,
+                  verticalSpacing + maxTileHeight),
             ],
           ),
         ],
       ),
     );
   }
-  Widget mouseRegion() {
 
+  Widget mouseRegion(int i, String title,double left, double top) {
     return Positioned(
-      top: verticalSpacing,
-      left: spacing,
+      left: left,
+      top: top,
       child: Container(
+        width: maxTileWidth,
+        height: maxTileHeight,
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.white)
+          color:
+              isHovered[i] ? Colors.pink.withOpacity(0.4) : Colors.transparent,
+          border: Border.all(
+            color: Colors.white,
+          ),
         ),
-        // color: hoverColor,
         child: MouseRegion(
           onEnter: (event) {
             setState(() {
-              hoverColor = Colors.pink.withOpacity(0.4);
-              // backgroundImage = hipaa();
+              backgroundImage = backgroundImages[i];
+              isHovered[i] = true;
             });
           },
           onExit: (event) {
             setState(() {
-              hoverColor = Colors.transparent;
+              // hoverColor[] = Colors.transparent;
+              isHovered[i] = false;
             });
           },
           onHover: (event) {},
           child: SizedBox(
             width: 150,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text('Health Tech'),
-                SizedBox(height: 20,),
+                 Text(title,
+                 style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400
+                 ),),
+                SizedBox(
+                  height: 20,
+                ),
                 Text(loremIpsum(words: 20, initWithLorem: true)),
               ],
             ),
@@ -92,7 +133,98 @@ mouseRegion(),
       child: Image.asset(
         'assets/images/health-tech.jpeg',
         width: double.infinity,
-        // height: 650, 
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered edTech() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/book.webp',
+        width: double.infinity,
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered retail() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/retail.jpg',
+        width: double.infinity,
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered aiDiag() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/diagnostics.jpg',
+        width: double.infinity,
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered greenTech() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/green.jpeg',
+        width: double.infinity,
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered iot() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/iot.jpg',
+        width: double.infinity,
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered finTech() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/fintech.webp',
+        width: double.infinity,
+        // height: 650,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  ColorFiltered hipaa() {
+    return ColorFiltered(
+      colorFilter:
+          ColorFilter.mode(Colors.grey.withOpacity(0.9), BlendMode.saturation),
+      child: Image.asset(
+        'assets/images/hipaa.jpeg',
+        width: double.infinity,
+        // height: 650,
         fit: BoxFit.contain,
       ),
     );
